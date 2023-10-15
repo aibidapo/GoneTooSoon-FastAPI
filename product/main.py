5b312579ff7d9548caa89bc8ed22dc15aa0ca04f
aibidapo <24958100+aibidapo@users.noreply.github.com>
2023-10-15 11:44:42 -0500

    Updated comments

--- product/main.py
+++ product/main.py
@@ -1,5 +1,6 @@
+# Import necessary modules and packages
 from fastapi import FastAPI, Response, HTTPException
-from sqlalchemy.sql.functions import mode 
+from sqlalchemy.sql.functions import mode
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from .import schemas
@@ -11,30 +12,35 @@ from passlib.context import CryptContext
 
 
 
+# Create a FastAPI application
 app = FastAPI(title="GoneTooSoon 🧙🏼 '\U0001F913' ")
 
+
+
+# Create database tables if they don't exist
 models.Base.metadata.create_all(engine)
 
+# Initialize a password hashing context
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+# Define a function to get a database session using context manager
 def get_db():
     db = SessionLocal()
-    
     try:
         yield db
-        
     finally:
         db.close()
-    
 
-
+# Define a route to get a list of products
 @app.get('/products', response_model=List[schemas.DisplayProduct])
 def products(db: Session = Depends(get_db)):
     products = db.query(models.Product).all()
     return products
 
 
+
+# Define a route to delete a product by its ID
 @app.delete('/product/{id}')
 def delete_product(id, db: Session = Depends(get_db)):
     db.query(models.Product).filter(models.Product.id == id).delete(synchronize_session=False)
@@ -43,33 +49,32 @@ def delete_product(id, db: Session = Depends(get_db)):
 
 
 
+# Define a route to get a product by its ID
 @app.get('/product/{id}', response_model=schemas.DisplayProduct)
 def product(id, response: Response, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id).first()
-    
     if not product:
         raise HTTPException(status_code=404, detail=f"Product {id} not found")
-        
     return product
-    
 
+
+
+# Define a route to update a product by its ID
 @app.put('/products/{id}')
-def update_product(id, request:schemas.Product, db: Session = Depends(get_db)):
+def update_product(id, request: schemas.Product, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id)
     if not product.first():
         pass
-    
     product.update(request.dict())
     db.commit()
     return {f'Product {id} successfully updated'}
-        
 
 
 
+# Define a route to add a new product
 @app.post('/product', status_code=status.HTTP_201_CREATED)
-def add(request:schemas.Product, db: Session = Depends(get_db)):
+def add(request: schemas.Product, db: Session = Depends(get_db)):
     new_product = models.Product(name=request.name, description=request.description, price=request.price)
-    
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
@@ -77,12 +82,13 @@ def add(request:schemas.Product, db: Session = Depends(get_db)):
 
 
 
-@app.post('/seller')
-def create_seller(request:schemas.Seller, db: Session = Depends(get_db)):
+# Define a route to create a new seller
+@app.post('/seller', response_model=schemas.DisplaySeller)
+def create_seller(request: schemas.Seller, db: Session = Depends(get_db)):
+    # Hash the seller's password for security
     hashedpassword = pwd_context.hash(request.password)
-    new_seller = models.Seller(username=request.username, email=request.email, 
-                               password=hashedpassword)
+    new_seller = models.Seller(username=request.username, email=request.email, password=hashedpassword)
     db.add(new_seller)
     db.commit()
     db.refresh(new_seller)
-    return request
+    return new_seller
--- product/schemas.py
+++ product/schemas.py
@@ -1,26 +1,44 @@
+# Importing the BaseModel class from Pydantic
 from pydantic import BaseModel
 
 
+# Defining a Pydantic model for the 'Product' entity
 class Product(BaseModel):
-    name: str
-    description: str
-    price: int
-    
-    
+    # Defining the fields for the 'Product' model
+    name: str         # Name of the product (string)
+    description: str  # Description of the product (string)
+    price: int        # Price of the product (integer)
+
+
+
+# Defining a Pydantic model for displaying 'Product' data
 class DisplayProduct(BaseModel):
-    name: str
-    description: str
-    id: int
-    
-    
+    name: str         # Name of the product (string)
+    description: str  # Description of the product (string)
+    id: int           # Identifier (ID) of the product (integer)
+
+    # Pydantic model configuration
     class Config:
-        orm_mode = True
-        
-        
-        
+        orm_mode = True  # Configuring 'orm_mode' to work with SQLAlchemy ORM
+
+
+
+# Defining a Pydantic model for the 'Seller' entity
 class Seller(BaseModel):
-    username: str
-    email: str
-    password: str
-    id: int
-            
+    # Defining the fields for the 'Seller' model
+    username: str     # Username of the seller (string)
+    email: str        # Email of the seller (string)
+    password: str     # Password of the seller (string)
+    id: int           # Identifier (ID) of the seller (integer)
+
+
+
+# Defining a Pydantic model for displaying 'Seller' data
+class DisplaySeller(BaseModel):
+    username: str     # Username of the seller (string)
+    email: str        # Email of the seller (string)
+    id: int           # Identifier (ID) of the seller (integer)
+
+    # Pydantic model configuration
+    class Config:
+        orm_mode = True  # Configuring 'orm_mode' to work with SQLAlchemy ORM
